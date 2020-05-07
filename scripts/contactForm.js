4423c854aa6d160c464c82babcb6821aab64755c
AzinT <ataqizad@uwaterloo.ca>
2020-05-07 03:19:15 -0400

    added projects + profile png+ updated footer

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -6,16 +6,16 @@ $(document).ready(function () {
     statusElm.empty();
 
     if (email.length > 5 && email.includes("@") && email.includes(".")) {
-      statusElm.append("<div>email is valid</div>");
+      // statusElm.append("<div>email is valid</div>");
     } else {
       event.preventDefault();
-      statusElm.append("<div>email is not valid</div>");
+      statusElm.append("<div>email is not valid, please try again.</div>");
     }
-    if (message.length > 2) {
-      statusElm.append("<div>message is valid</div>");
+    if (message.length > 1) {
+      // statusElm.append("<div>message is valid</div>");
     } else {
       event.preventDefault();
-      statusElm.append("<div>message is not valid</div>");
+      statusElm.append("<div>message must be more than 1 character.</div>");
     }
   });
 });
