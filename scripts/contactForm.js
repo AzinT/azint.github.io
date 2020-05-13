3b4045140bb86be407d695b25a30a80bdabb7f37
AzinT <39173615+AzinT@users.noreply.github.com>
2020-05-13 08:45:12 -0400

    Update contactForm.js

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,21 +1,2 @@
-$(document).ready(function () {
-  $(".contact-form-btn").click(function (event) {
-    var email = $(".email").val();
-    var message = $(".message").val();
-    var statusElm = $(".status");
-    statusElm.empty();
-
-    if (email.length > 5 && email.includes("@") && email.includes(".")) {
-      // statusElm.append("<div>email is valid</div>");
-    } else {
-      event.preventDefault();
-      statusElm.append("<div>email is not valid, please try again.</div>");
-    }
-    if (message.length > 1) {
-      // statusElm.append("<div>message is valid</div>");
-    } else {
-      event.preventDefault();
-      statusElm.append("<div>message must be more than 1 character.</div>");
-    }
-  });
-});
+$(document).ready(function(){$(".contact-form-btn").click(function(event){var email=$(".email").val();var message=$(".message").val();var statusElm=$(".status");statusElm.empty();if(email.length>5&&email.includes("@")&&email.includes(".")){}else{event.preventDefault();statusElm.append("<div>email is not valid, please try again.</div>");}
+if(message.length>1){}else{event.preventDefault();statusElm.append("<div>message must be more than 1 character.</div>");}});});
